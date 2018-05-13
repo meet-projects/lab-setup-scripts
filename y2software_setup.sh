dd108310b48748b1219567013d6acb5d384c487f
golfit <einav19@meet.mit.edu>
2018-05-13 18:27:59 -0400

    Adding list of requirements to Y2 setup script.

--- y2software_setup.sh
+++ y2software_setup.sh
@@ -1,12 +1,13 @@
 
 #!/bin/sh
-apt-get update -y
+sudo apt-get update -y
+
+#Note: Don't upgrade pip to Version 10 in Ubuntu 16.04, as of 13 May 2018, unless you want to sort out conflict between user-version of pip and system-wide version.  Version 8.1 is okay
 
 #install inkscape
 sudo add-apt-repository ppa:inkscape.dev/stable
 #sudo apt-get update
 sudo apt install inkscape -y
-
 
 #slack
 sudo apt-add-repository -y ppa:rael-gc/scudcloud
@@ -29,3 +30,9 @@ sudo apt-get install kdenlive -y
 sudo apt install kdenlive -yk
 sudo ppa-purge ppa:kdenlive/kdenlive-stable
 
+#Install virtual environment
+sudo apt-get install virtualenv
+
+#Install list of requirements
+sudo pip install -r y2requirements.txt
+sudo pip3 install -r y2requirements.txt
