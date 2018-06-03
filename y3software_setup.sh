c33bb6c7a590658cb22d84ed78a973a2d9d946f8
golfit <golfit@mit.edu>
2018-06-03 19:16:50 -0400

    Forgot to install React native

--- y3software_setup.sh
+++ y3software_setup.sh
@@ -6,6 +6,7 @@
 echo "================================"
 echo "Note: the Y2 software setup should be run first, as Y3 has the same dependencies"
 
+sudo npm install -g react-native-cli #Install React native
 sudo npm install -g ionic cordova #Install ionic framework and cordova  - (for building apps)
 sudo pip install jupyterlab #Install Jupyter lab
 sudo pip3 install jupyterlab #Install Jupyter lab
