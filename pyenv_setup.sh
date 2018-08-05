175d2ed4328afb717c6669d3f1f6b13427fc5d33
Emily Liu <emiliu@users.noreply.github.com>
2018-08-05 10:16:15 +0300

    add pyenv dependencies and pylint

--- pyenv_setup.sh
+++ pyenv_setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 cd ~
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
+libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+xz-utils tk-dev libffi-dev
 curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
 echo "export PATH=\"~/.pyenv/bin:\$PATH\"" >> ~/.bashrc
 echo "eval \"\$(pyenv init -)\"" >> ~/.bashrc
@@ -14,3 +17,4 @@ echo "SQLAlchemy==1.1.11" >> requirements.txt
 pip install --upgrade pip
 pip install -r requirements.txt
 rm requirements.txt
+sudo apt-get install -y pylint
